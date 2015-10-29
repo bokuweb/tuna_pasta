fe1c90a1d57622600141bbe41aed0fca607e6e5d
bokuweb <info@typebeats.com>
2015-10-29 12:12:10 +0900

    add infinite scroll skeleton

--- src/components/pasta.js
+++ src/components/pasta.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import Infinite from 'react-infinite';
 
+
+var i = 0
+
 export default class Pasta extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
     this.props.fetchFeed('http://b.hatena.ne.jp/hotentry.rss');
+    this.state = {
+      isInfiniteLoading : false
+    }
+  }
+
+  handleInfiniteLoad() {
+    console.log("load");
+    this.setState({isInfiniteLoading: true});
+    setTimeout(() => {
+      for(let j = 0; j < 100; j++)
+        this.props.items.push({link:"a" + j, title:"hogasdasdasdasddase" + j});
+      i++;
+      this.setState({
+        isInfiniteLoading: false,
+      });
+    }, 2500);
+
+  }
+
+  elementInfiniteLoad() {
+    return <div className="infinite-list-item">
+      Loading...
+      </div>;
   }
 
   render() {
-    const items = this.props.items.map((item, index) => {
+    const innerHeight = document.documentElement.clientHeight;
+    const items = this.props.items.map((item) => {
       return (
-          <div>{item.title}</div>
+          <div className="item" key={item.link}>{item.title}</div>
       );
     });
     return (
@@ -18,14 +46,13 @@ export default class Pasta extends Component {
           a
         </div>
         <div id="content">
-        <Infinite elementHeight={40}
-             containerHeight={440}
-             infiniteLoadBeginBottomOffset={200}
-             //onInfiniteLoad={this.handleInfiniteLoad}
-             //loadingSpinnerDelegate={this.elementInfiniteLoad()}
-             //isInfiniteLoading={this.state.isInfiniteLoading}
-             className="item"
-            >
+        <Infinite
+          elementHeight={10}
+          containerHeight={innerHeight}
+          infiniteLoadBeginBottomOffset={500}
+          onInfiniteLoad={this.handleInfiniteLoad.bind(this)}
+          loadingSpinnerDelegate={this.elementInfiniteLoad()}
+          isInfiniteLoading={this.state.isInfiniteLoading} >
         {items}
       </Infinite>
         </div>
