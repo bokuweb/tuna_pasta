fda663e68644e9042f35e8d9ee36e4c667585e56
bokuweb <info@typebeats.com>
2015-11-01 17:41:51 +0900

    implement default category filter

--- src/actions/menu.js
+++ src/actions/menu.js
@@ -1,11 +1,12 @@
+import _ from 'lodash';
 import * as types from '../constants/action-types';
 import {categories} from '../constants/categories';
 
 export function onSelectKeyword(keyword) {
-  console.log(keyword);
+  const keywordObj = _.filter(categories, (category) => category.name === keyword)[0];
   return {
     type: types.SELECT_KEYWORD,
-    keyword : keyword,
+    keyword : keywordObj,
     isDefault : true
   };
 }
