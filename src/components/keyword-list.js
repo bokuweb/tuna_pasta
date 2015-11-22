2e3d63cd47ca7eb21daebaf372fea81007e27041
bokuweb <info@typebeats.com>
2015-11-22 10:18:17 +0900

    [refactor] Refactoring Keyword-list style

--- src/components/keyword-list.js
+++ src/components/keyword-list.js
@@ -16,15 +16,15 @@ export default class KeywordList extends Component {
   getKeywordList() {
     const {activeKeyword, keywords} = this.props;
     return keywords.map((keyword) => {
-      const listClassName = keyword.name === activeKeyword ? 'selected' : '';
+      const listClassName = keyword.name === activeKeyword ? 'keywords__list keywords__list--selected' : 'keywords__list';
       return (
         <li className={listClassName} key={keyword.name}>
           <div onClick={this.onSelect.bind(this, keyword.name)}>
-            <i className={"fa fa-" + keyword.icon} />
+            <i className={"keywords__icon fa fa-" + keyword.icon} />
             {keyword.name}
           </div>
           <div className="remove" onClick={this.onRemove.bind(this, keyword.name)} >
-            <i className={"fa fa-close"} />
+            <i className={"keywords__icon fa fa-close"} />
           </div>
         </li>
       );
@@ -34,15 +34,15 @@ export default class KeywordList extends Component {
   render() {
     const {activeKeyword} = this.props;
     return (
-      <div id="menu">
-        <ul>
-          <li className={activeKeyword === 'all' ? 'selected' : ''}
+      <div className="keywords" id="menu">
+        <ul className="keywords__ul">
+          <li className={activeKeyword === 'all' ? 'keywords__list keywords__list--selected' : 'keywords__list'}
             onClick={this.onSelect.bind(this, 'all')}>
-            <div><i className={"fa fa-home"} />総合</div>
+            <div><i className={"keywords__icon fa fa-home"} />総合</div>
          </li>
-         <li className={activeKeyword === 'favorite' ? 'selected' : ''}
+          <li className={activeKeyword === 'favorite' ? 'keywords__list keywords__list--selected' : 'keywords__list'}
            onClick={this.onSelect.bind(this, 'favorite')}>
-           <div><i className={"fa fa-heart"} />お気に入り</div>
+           <div><i className={"keywords__icon fa fa-heart"} />お気に入り</div>
          </li>
          {this.getKeywordList()}
        </ul>
--- test/components/test-keyword-list.js
+++ test/components/test-keyword-list.js
@@ -16,28 +16,23 @@ describe('KeywordList Component test', () => {
         <KeywordList activeKeyword={'react'} keywords={keywords}/>);
     const list = TestUtils.scryRenderedDOMComponentsWithTag(keywordList, 'li');
     assert.equal(list[0].textContent, '総合');
-    assert.equal(list[0].className, '');
+    assert.equal(list[0].className, 'keywords__list');
     assert.equal(list[1].textContent, 'お気に入り');
-    assert.equal(list[1].className, '');
+    assert.equal(list[1].className, 'keywords__list');
     assert.equal(list[2].textContent, 'react');
-    assert.equal(list[2].className, 'selected');
+    assert.equal(list[2].className, 'keywords__list keywords__list--selected');
     assert.equal(list[3].textContent, 'redux');
-    assert.equal(list[3].className, '');
+    assert.equal(list[3].className, 'keywords__list');
     done();
   });
 
   it ('Should call onSelect handler when click keyword', (done) => {
+    const handleOnSelect = name => {
+
+    };
     const keywordList = TestUtils.renderIntoDocument(
-        <KeywordList activeKeyword={'react'} keywords={keywords}/>);
+      <KeywordList activeKeyword={'react'} keywords={keywords} onSelect={handleOnSelect}/>);
     const list = TestUtils.scryRenderedDOMComponentsWithTag(keywordList, 'li');
-    assert.equal(list[0].textContent, '総合');
-    assert.equal(list[0].className, '');
-    assert.equal(list[1].textContent, 'お気に入り');
-    assert.equal(list[1].className, '');
-    assert.equal(list[2].textContent, 'react');
-    assert.equal(list[2].className, 'selected');
-    assert.equal(list[3].textContent, 'redux');
-    assert.equal(list[3].className, '');
     done();
   });
 
