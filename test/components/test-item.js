d9382e7ecf7858eceaf94bdec5939013567dd39a
bokuweb <info@typebeats.com>
2015-11-23 23:26:57 +0900

    add item test skeleton

--- test/components/test-item.js
+++ test/components/test-item.js
@@ -6,9 +6,9 @@ import Item from '../../src/components/item';
 
 describe('Item Component test', () => {
   it ('Should display close icon, when menu open', (done) => {
-    const header = TestUtils.renderIntoDocument(<Header isMenuOpen={true} />);
-    const icons = TestUtils.scryRenderedDOMComponentsWithTag(header, 'i');
-    assert(icons[0].className, 'fa fa-close')
+    //const header = TestUtils.renderIntoDocument(<Header isMenuOpen={true} />);
+    //const icons = TestUtils.scryRenderedDOMComponentsWithTag(header, 'i');
+    //assert(icons[0].className, 'fa fa-close')
     done();
   });
 
