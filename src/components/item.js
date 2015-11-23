24aa8d9eb09aad61034ac051592cae431da3767c
bokuweb <info@typebeats.com>
2015-11-23 17:44:11 +0900

    [refactor] apply BEM to item.styl

--- src/components/item.js
+++ src/components/item.js
@@ -30,7 +30,7 @@ export default class item extends Component {
   getCategories(categories) {
     return categories.map((category) => {
       return (
-        <span className="category"
+        <span className="item__category"
               key={category + this.props.activeKeyword }
               style={{'backgroundColor':'#1ABC9C'}}>
           {category}
@@ -39,7 +39,8 @@ export default class item extends Component {
   }
 
   getCommentButton(item) {
-    const icon = item.isCommentFetching? "fa fa-spinner fa-spin" : "fa fa-commenting";
+    const icon = item.isCommentFetching ? "item__icon--comment fa fa-spinner fa-spin"
+                                        : "item__icon--comment fa fa-commenting";
     const text = item.isCommentOpen? "コメントを閉じる" : "コメントを見る";
     return (
       <div className="comment-button" onClick={this.onCommentClick.bind(this, item)}>
@@ -56,14 +57,16 @@ export default class item extends Component {
     const favoriteButtonClass = item.isFavorited? "favorite-button favorited" : "favorite-button";
     return (
       <div id={id} className="item animated fadeIn">
-        <img className="favicon" src={favicon} alt="favicon" />
-        <a href={item.link} target="blank" className="item-title">{item.title}</a>
-        <a href={hatebuHref} className="hatebu"><img src={hatebuImage} alt="" /></a><br />
-        <span className="publish-date">{item.publishedDate}</span>
+        <img className="item__favicon" src={favicon} alt="favicon" />
+        <a href={item.link} target="blank" className="item__title">{item.title}</a>
+        <a href={hatebuHref} className="item__hatebu--link">
+          <img src={hatebuImage} alt="" className="item__hatebu--image" />
+        </a><br />
+        <span className="item__publish-date">{item.publishedDate}</span>
         {this.getCategories(item.categories)}<br />
-        <p className="content-snippet">{unescapeHTML(item.contentSnippet)}</p>
+        <p className="item__content-snippet">{unescapeHTML(item.contentSnippet)}</p>
         <div className={favoriteButtonClass} onClick={this.onFavoriteClick.bind(this, item)}>
-          <i className="fa fa-heart" />お気に入り
+          <i className="item__button--favorite fa fa-heart" />お気に入り
         </div>
         {this.getCommentButton(item)}
         <div className={(item.isCommentOpen) ? "comment-box comment-box-open": "comment-box comment-box-close"}>
